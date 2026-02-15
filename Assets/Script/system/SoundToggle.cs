5730391d2994ae2a3334e0dd6689de1a52b25963
Châu Minh Duy <chauminhduy.2607@gmail.com>
2026-02-15 23:04:40 +0700

    change script to on off sound

--- Assets/Script/system/SoundToggle.cs
+++ Assets/Script/system/SoundToggle.cs
@@ -11,32 +11,86 @@ public class SoundToggle : MonoBehaviour
     [SerializeField] private GameObject onSoundTick;
     [SerializeField] private GameObject offSoundTick;
     
+    [Header("=== AUDIO ===")]
+    [SerializeField] private AudioClip clickSound;
+    
     [Header("=== SETTINGS ===")]
     [SerializeField] private bool isSoundOn = true;
 
     private void Start()
     {
+        // Load setting đã lưu
+        isSoundOn = PlayerPrefs.GetInt("IsMusicOn", 1) == 1;
+        
         onSoundButton.onClick.AddListener(OnClickSoundOn);
         offSoundButton.onClick.AddListener(OnClickSoundOff);
         
         UpdateUI();
+        ApplyMusicSetting();
     }
 
     private void OnClickSoundOn()
     {
+        PlayClickSound();
         isSoundOn = true;
+        PlayerPrefs.SetInt("IsMusicOn", 1);
+        PlayerPrefs.Save();
         UpdateUI();
+        ApplyMusicSetting();
     }
 
     private void OnClickSoundOff()
     {
+        PlayClickSound();
         isSoundOn = false;
+        PlayerPrefs.SetInt("IsMusicOn", 0);
+        PlayerPrefs.Save();
         UpdateUI();
+        ApplyMusicSetting();
     }
 
     private void UpdateUI()
     {
         onSoundTick.SetActive(isSoundOn);
         offSoundTick.SetActive(!isSoundOn);
+    }
+    
+    private void ApplyMusicSetting()
+    {
+        // Tìm tất cả AudioSource trong game
+        AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>();
+        
+        foreach (AudioSource audio in allAudioSources)
+        {
+            // Chỉ ảnh hưởng nhạc nền (có loop = true)
+            // Không ảnh hưởng âm thanh nút (loop = false)
+            if (audio.loop)
+            {
+                if (isSoundOn)
+                {
+                    audio.volume = 1f;
+                    if (!audio.isPlaying)
+                        audio.Play();
+                }
+                else
+                {
+                    audio.volume = 0f;
+                }
+            }
+        }
+    }
+    
+    private void PlayClickSound()
+    {
+        if (clickSound != null)
+        {
+            // Tạo GameObject tạm để phát âm thanh click
+            GameObject tempAudio = new GameObject("TempClickAudio");
+            AudioSource audioSource = tempAudio.AddComponent<AudioSource>();
+            audioSource.clip = clickSound;
+            audioSource.loop = false;
+            audioSource.Play();
+            Destroy(tempAudio, clickSound.length);
+        }
     }
 }
